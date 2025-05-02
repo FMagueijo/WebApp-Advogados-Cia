aebc2f1fba8a3f1248108889bef38cb73670c651
davidvieira51756-code <davidvieira51756@gmail.com>
2025-05-02 22:25:12 +0100

    Alterar Estado

--- src/app/(caso)/caso/[id]/actions.ts
+++ src/app/(caso)/caso/[id]/actions.ts
@@ -7,6 +7,7 @@ interface CasoProfile {
   id?: number;
   processo: string;
   resumo: string;
+  estado: string;
   descricao?: string;
 }
 
@@ -17,6 +18,7 @@ export async function fetchCasoProfile(casoId: number): Promise<CasoProfile | nu
       select: {
         id: true,
         processo: true,
+        estado: true,
         resumo: true, 
         descricao: true,
       }
@@ -29,11 +31,31 @@ export async function fetchCasoProfile(casoId: number): Promise<CasoProfile | nu
     return {
         id: user.id,
         processo: user.processo,
+        estado: user.estado.nome_estado,
         resumo: user.resumo,
         descricao: user.descricao as string,
     };
   } catch (error) {
     console.error('Database error:', error);
     return null;
+  }
+}
+
+export async function updateCasoEstado(casoId: number, novoEstado: string): Promise<boolean> {
+  try {
+    await prisma.caso.update({
+      where: { id: casoId },
+      data: {
+        estado: {
+          connect: { nome_estado: novoEstado },
+        },
+      },
+    });
+
+    revalidatePath(`/caso/${casoId}`);
+    return true;
+  } catch (error) {
+    console.error('Error updating caso estado:', error);
+    return false;
   }
 }
