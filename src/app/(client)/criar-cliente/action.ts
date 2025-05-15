fad9153fce364ed696267422c4288f87e5127606
davidvieira51756-code <davidvieira51756@gmail.com>
2025-05-15 09:44:42 +0100

    mudanca bd e criar utilizador com divida

--- src/app/(client)/criar-cliente/action.ts
+++ src/app/(client)/criar-cliente/action.ts
@@ -1,4 +1,4 @@
- 'use server';
+'use server';
 
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache"; // se quiser atualizar algum caminho
@@ -19,7 +19,16 @@ export async function criarCliente(data: ClienteData) {
         email: data.email,
         telefone: data.telefone,
         codigoPostal: data.codigoPostal,
-        endereco: data.endereco
+        endereco: data.endereco,
+        dividas: {
+          create: {
+            valor: 0, // Valor inicial da dívida
+            pago: false // Status inicial
+          }
+        }
+      },
+      include: {
+        dividas: true // Opcional: inclui os dados da dívida criada no retorno
       }
     });
 
@@ -30,4 +39,5 @@ export async function criarCliente(data: ClienteData) {
   } catch (error) {
     console.error("Erro ao criar cliente:", error);
     throw new Error("Erro ao criar cliente.");
-  }}
+  }
+}
